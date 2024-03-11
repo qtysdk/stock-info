f225e880f60838861ed065f5686b72455abf38d1
Ching Yi, Chan <qrtt1@infuseai.io>
2024-03-11 20:01:51 +0800

    Make request and parser configurable
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

--- stock_info/downloader.py
+++ stock_info/downloader.py
@@ -3,8 +3,11 @@ import os
 import logging
 import re
 from dataclasses import dataclass
+from typing import Callable, Dict
 
 import requests
+
+from stock_info.recipe import find_parser, find_request_template
 
 logger = logging.getLogger(__name__)
 
@@ -16,6 +19,28 @@ class Result:
     dividend: float
     exDividendDate: str
     dividendPaymentDate: str
+
+
+@dataclass
+class RequestParameters:
+    url: str
+    headers: Dict
+
+
+def create_request_parameters_from_curl_command(curl_command: str) -> RequestParameters:
+    lines = re.findall(r"(curl|-H) '([^']+)'.*", curl_command)
+    url = None
+    headers = dict()
+    for line in lines:
+        data_type, value = line
+        if data_type == "curl":
+            url = value
+        if data_type == "-H":
+            parsed = re.findall(r"(^[^:]+): (.+)", value)
+            if parsed:
+                header_name, header_value = parsed[0]
+                headers[header_name] = header_value
+    return RequestParameters(url=url, headers=headers)
 
 
 def build_failed_result(key: str):
@@ -35,56 +60,41 @@ class Downloader:
         logger.info(f"is-fake-enabled: {self.enable_fake}")
 
     def download(self, key: str):
-        text = self.http_request_for(key)
+        text = None
+        if self.enable_fake:
+            text = self.use_previous_data(key)
+        else:
+            text = self.http_request_for(key)
         if text:
-            return self.parse_for(key, text)
-
+            parse_func: Callable[[str, str], Result] = find_parser(key)
+            return parse_func(key, text)
         return build_failed_result(key)
 
-    def http_request_for(self, key):
-        # TODO fix the hardcode 0056 (1084)
+    def use_previous_data(self, key: str):
+        import stock_info
+        import pathlib
 
-        curl_command = r"""
-curl 'https://api.yuantafunds.com/ectranslation/api/trans?APIType=EC2API&AppName=FundWeb&PageName=%2Fmyfund%2Fdividend&Device=4&DeviceId=cbe260a5-df5f-4a3e-a2fe-458b27c8b8e3&FuncId=FundDividend&FundId=1084&SDate=20240101&EDate=20241231&FundType=ETF' \
-  -H 'authority: api.yuantafunds.com' \
-  -H 'accept: application/json, text/plain, */*' \
-  -H 'accept-language: en-US,en;q=0.9,zh-TW;q=0.8,zh-CN;q=0.7,zh;q=0.6' \
-  -H 'cache-control: no-cache' \
-  -H 'origin: https://www.yuantafunds.com' \
-  -H 'pragma: no-cache' \
-  -H 'sec-ch-ua: "Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"' \
-  -H 'sec-ch-ua-mobile: ?0' \
-  -H 'sec-ch-ua-platform: "macOS"' \
-  -H 'sec-fetch-dest: empty' \
-  -H 'sec-fetch-mode: cors' \
-  -H 'sec-fetch-site: same-site' \
-  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
-        """
+        test_dir = pathlib.Path(stock_info.__file__).parent.parent / "tests"
+        testdata_dir = test_dir / "data"
+        testdata_dir.mkdir(exist_ok=True)
+        testdata = testdata_dir / f"{key}.data"
 
-        lines = re.findall(r"(curl|-H) '([^']+)'.*", curl_command)
-        url = None
-        headers = dict()
-        for line in lines:
-            data_type, value = line
-            if data_type == "curl":
-                url = value
-            if data_type == "-H":
-                parsed = re.findall(r"(^[^:]+): (.+)", value)
-                if parsed:
-                    header_name, header_value = parsed[0]
-                    headers[header_name] = header_value
+        if (
+            "true" == os.environ.get("MAKE_NEW_TESTDATA", "false")
+            or not testdata.exists()
+        ):
+            text = self.http_request_for(key)
+            with open(testdata, "w") as fh:
+                fh.write(text)
+            return text
 
-        return requests.get(url, headers=headers).text
+        if testdata.exists():
+            logger.warning(f"Load from testdata for {key}")
+            with open(testdata, "r") as fh:
+                return fh.read()
 
-    def parse_for(self, key: str, text: str):
-        # TODO fix the ignored key
-        data = json.loads(text)
-        data = data["Data"]["Data"][0]
-
-        return Result(
-            success=True,
-            stock_number=key,
-            dividend=data["DIVIDEN_PER_UNIT"],
-            exDividendDate=data["SHARE_DATE"],
-            dividendPaymentDate=data["PAY_DATE"],
+    def http_request_for(self, stock_number: str):
+        params = create_request_parameters_from_curl_command(
+            find_request_template(stock_number)
         )
+        return requests.get(params.url, headers=params.headers).text
